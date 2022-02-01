c6e1fa7401a9f9a6193a239c044f94cd54a62029
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-01-31 19:51:14 -0600

    Made Component stubs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Carousel from './components/Carousel';
+import Gallery from './components/Gallery';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/Gallery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GalleryItem from './GalleryItem';
+
+const Gallery = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Gallery;
